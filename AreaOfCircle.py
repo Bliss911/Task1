543ce6ec75a9efeb1cb95f8ae74497c32eca64db
Onwukanjo Innocent O <innociousbliss@gmail.com>
2020-03-26 06:40:56 +0100

    Complete Build of AreaOfCircle

---
+++ AreaOfCircle.py
@@ -0,0 +1,4 @@
+print("========== Area of Circle Calculator ============\n")
+radius = input("Enter the value for radius: ")
+area = 3.142 *float(radius) * float(radius)
+print ("\nThe area of circle with radius " +radius+ " is " +str(area))
